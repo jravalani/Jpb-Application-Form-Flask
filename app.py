b96eb9e52d85f57edaa41a1b2668dd6a73c9e7c8
Jay Ravalani <jay.ravalani746@gmail.com>
2024-08-24 08:17:27 +0530

    connect to postgres

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("FLASK_PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_DATABASE_URI")
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,4 +35,7 @@ def index():
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
